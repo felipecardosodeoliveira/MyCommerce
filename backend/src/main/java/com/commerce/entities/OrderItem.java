565413f152517d77640f826330e51353fc25aed0
felipe <felipecardosodeoliveira02@gmail.com>
2025-09-10 23:31:34 -0300

    feat(main): criando estrutura do banco

---
+++ backend/src/main/java/com/commerce/entities/OrderItem.java
@@ -0,0 +1,58 @@
+package com.commerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+    @EmbeddedId
+    OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {}
+
+    public OrderItem(OrderItemPK id, Integer quantity, Double price) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public OrderItemPK getId() {
+        return id;
+    }
+
+    public void setId(OrderItemPK id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
